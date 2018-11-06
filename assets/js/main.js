a85a80cef397b00aebc7a303de23244466dfe93d
Raj Mehrotra <rajeev.a.mehrotra1@gmail.com>
2018-11-05 23:21:30 -0800

    add smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,19 @@
   $(document).click(collapseNavbar);
   $(document).scroll(collapseNavbar);
 
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Store hash
+    var hash = this.hash;
+    var scrollTo = $(hash).offset() ? $(hash).offset().top : 0;
+    $('html, body').animate({
+      scrollTop:  scrollTo
+    }, 800, function(){
+
+      window.location.hash = hash;
+    });
+  });
+
 }());
 
 function collapseNavbar(e) {
